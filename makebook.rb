5e4efd186aa834861d8c73018c66874eefce6167
Alexander Standke <xanderstrike@gmail.com>
2014-09-05 22:26:26 -0700

    make presentable

--- makebook.rb
+++ makebook.rb
@@ -1,3 +1,26 @@
+# makebook.rb
+#  alex standke - 2014
+#   takes odd and even photos and turns them into a book
+#   works well with diy book scanners
+
+# variables
+
+odd_rotation = 90
+odd_x_offset = 912
+odd_y_offset = 669
+odd_x_crop = 3160
+odd_y_crop = 2384
+
+even_rotation = 270
+even_x_offset = 528
+even_y_offset = 576
+even_x_crop = 3280
+even_y_crop = 2520
+
+resize = 25 # percent
+
+# here we go
+
 `mkdir combined`
 
 puts 'starting odd pages'
@@ -6,7 +29,7 @@ n = 1
 files.each do |f|
   number = n.to_s.rjust(5, "0")
   `cp odd/#{f} combined/#{number}.jpg`
-  `mogrify -crop 3160x2384+912+669 -rotate 90 combined/#{number}.jpg`
+  `mogrify -crop #{odd_x_crop}x#{odd_y_crop}+#{odd_x_offset}+#{odd_y_offset} -rotate #{odd_rotation} combined/#{number}.jpg`
   n += 2
   puts "moved #{f}"
 end
@@ -17,13 +40,19 @@ n = 0
 files.each do |f|
   number = n.to_s.rjust(5, "0")
   `cp even/#{f} combined/#{number}.jpg`
-  `mogrify -crop 3280x2520+528+576 -rotate 270 combined/#{number}.jpg`
+  `mogrify -crop #{even_x_crop}x#{even_y_crop}+#{even_x_offset}+#{even_y_offset} -rotate #{even_rotation} combined/#{number}.jpg`
   n += 2
   puts "moved #{f}"
 end
 
 puts 'final processing'
-`mogrify -resize 25% combined/*.jpg`
+`mogrify -resize #{resize}% combined/*.jpg`
 
 puts 'outputting pdf'
 `convert -limit memory 1 -limit map 1 combined/*.jpg book.pdf`
+
+print 'clean up? [yN] '
+gets r
+`rm -rf combined` r.downcase == 'y'
+
+puts 'all done, look at book.pdf'
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # bookscanner
 
 This is a script I'm working on to automate processing of diy-bookscanner'd books.
+
+#### usage
+
+Put your images into `odd` and `even` folders, if you used a two camera DIY scanner they should already be separate. 
+
+Modify `makebook.rb` to suit your needs, you can find out your crop offset and size using GIMP. 
+
+Change resize to suit the resolution of your cameras, this pretty much directly determines the size of your PDF, too large and your PDF can be huge (gigabytes), too small and you will lose detail.
+
+Once you are ready, simply run it with `ruby makebook.rb`. You do have ruby installed... right?
